425f1723b1a7e88bcd59d6339a0a18fdfbea2312
Максим Медяник <tvoygluk@gmail.com>
2020-08-30 00:02:58 +0300

    Connected with firebase

---
+++ src/components/UI/Loader/Loader.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import classes from './Loader.module.scss'
+
+const Loader = () => {
+    return (
+        <div className={classes['loadingio-spinner-wedges-bupquaychks']}>
+            <div className={classes['ldio-ai8pfomxtc']}>
+                <div>
+                    <div>
+                        <div/>
+                    </div>
+                    <div>
+                        <div/>
+                    </div>
+                    <div>
+                        <div/>
+                    </div>
+                    <div>
+                        <div/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Loader;
--- src/pages/Quiz/Quiz.js
+++ src/pages/Quiz/Quiz.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import classes from './Quiz.module.scss'
 import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 import FinishedQuiz from "../../components/FinishedQuiz/FinishedQuiz";
+import Loader from "../../components/UI/Loader/Loader";
 
 class Quiz extends Component {
     /* answerState: { [id]: 'success' } либо answerState: { [id]: 'error' } */
@@ -10,63 +12,77 @@ class Quiz extends Component {
         activeQuestion: 0,
         answerState: null,
         answers: {},
-        quiz: [
-            {
-                id: 1,
-                rightAnswerId: 4,
-                question: 'Что выведет консоль?',
-                answers: [
-                    {text: '10', id: 1},
-                    {text: '91', id: 2},
-                    {text: '"4321"', id: 3},
-                    {text: '"91"', id: 4}
-                ],
-            },
-            {
-                id: 2,
-                rightAnswerId: 3,
-                question: 'Что выведет консоль?',
-                answers: [
-                    {text: '10', id: 1},
-                    {text: 'function', id: 2},
-                    {text: '1', id: 3},
-                    {text: 'TypeError', id: 4}
-                ],
-            },
-            {
-                id: 3,
-                rightAnswerId: 4,
-                question: 'Что выведет консоль?',
-                answers: [
-                    {text: '4', id: 1},
-                    {text: '3', id: 2},
-                    {text: '2', id: 3},
-                    {text: 'undefined', id: 4}
-                ],
-            },
-            {
-                id: 4,
-                rightAnswerId: 2,
-                question: 'В каком порядке выведутся логи?',
-                answers: [
-                    {text: '0231', id: 1},
-                    {text: '0213', id: 2},
-                    {text: '1302', id: 3},
-                    {text: '0123', id: 4}
-                ],
-            },
-            {
-                id: 5,
-                rightAnswerId: 1,
-                question: 'Что выведет консоль?',
-                answers: [
-                    {text: '[AsyncFunction: bar]', id: 1},
-                    {text: 'foo', id: 2},
-                    {text: '[AsyncFunction: foo]', id: 3},
-                    {text: 'bar', id: 4}
-                ],
-            },
-        ],
+        quiz: [],
+        hadQuizState: false,
+        // quiz: [
+        //     {
+        //         id: 1,
+        //         rightAnswerId: 4,
+        //         question: 'Что выведет консоль?',
+        //         answers: [
+        //             {text: '10', id: 1},
+        //             {text: '91', id: 2},
+        //             {text: '"4321"', id: 3},
+        //             {text: '"91"', id: 4}
+        //         ],
+        //     },
+        //     {
+        //         id: 2,
+        //         rightAnswerId: 3,
+        //         question: 'Что выведет консоль?',
+        //         answers: [
+        //             {text: '10', id: 1},
+        //             {text: 'function', id: 2},
+        //             {text: '1', id: 3},
+        //             {text: 'TypeError', id: 4}
+        //         ],
+        //     },
+        //     {
+        //         id: 3,
+        //         rightAnswerId: 4,
+        //         question: 'Что выведет консоль?',
+        //         answers: [
+        //             {text: '4', id: 1},
+        //             {text: '3', id: 2},
+        //             {text: '2', id: 3},
+        //             {text: 'undefined', id: 4}
+        //         ],
+        //     },
+        //     {
+        //         id: 4,
+        //         rightAnswerId: 2,
+        //         question: 'В каком порядке выведутся логи?',
+        //         answers: [
+        //             {text: '0231', id: 1},
+        //             {text: '0213', id: 2},
+        //             {text: '1302', id: 3},
+        //             {text: '0123', id: 4}
+        //         ],
+        //     },
+        //     {
+        //         id: 5,
+        //         rightAnswerId: 1,
+        //         question: 'Что выведет консоль?',
+        //         answers: [
+        //             {text: '[AsyncFunction: bar]', id: 1},
+        //             {text: 'foo', id: 2},
+        //             {text: '[AsyncFunction: foo]', id: 3},
+        //             {text: 'bar', id: 4}
+        //         ],
+        //     },
+        // ],
+    }
+
+    async componentDidMount() {
+        try {
+            const quiz = await axios.get('https://quiz-o-frontend.firebaseio.com/quiz/-MFvk33VE5f7pDHlCHPt.json');
+            this.setState({
+                quiz: quiz.data,
+                hadQuizState: true
+            })
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     isFinishedQuiz = () => {
@@ -134,7 +150,7 @@ class Quiz extends Component {
         return (
             <div className={classes.Quiz}>
                 <div className={classes.QuizWrapper}>
-                    <h1>Quiz</h1>
+                    <h1>Quiz-o-Frontend</h1>
 
                     {this.state.isFinished
                         ?
@@ -143,15 +159,18 @@ class Quiz extends Component {
                                 onRetryClick={this.onRetryClick}
                                 answers={this.state.answers}
                             />
-                        :
-                            <ActiveQuiz
-                                quizLength={this.state.quiz.length}
-                                question={this.state.quiz[this.state.activeQuestion].question}
-                                activeQuestion={this.state.activeQuestion + 1}
-                                answers={this.state.quiz[this.state.activeQuestion].answers}
-                                onAnswerClick={this.onAnswerClick}
-                                answerState={this.state.answerState}
-                            />
+                        : this.state.hadQuizState
+                            ?
+                                <ActiveQuiz
+                                    quizLength={this.state.quiz.length}
+                                    question={this.state.quiz[this.state.activeQuestion].question}
+                                    activeQuestion={this.state.activeQuestion + 1}
+                                    answers={this.state.quiz[this.state.activeQuestion].answers}
+                                    onAnswerClick={this.onAnswerClick}
+                                    answerState={this.state.answerState}
+                                />
+                            :
+                                <Loader />
                     }
                 </div>
             </div>
