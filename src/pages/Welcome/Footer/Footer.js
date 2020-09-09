5010691b9844e66e9075c3a355576f5ca887b85d
Максим Медяник <tvoygluk@gmail.com>
2020-09-10 00:09:12 +0300

    Changed html height

--- src/pages/Welcome/Footer/Footer.js
+++ src/pages/Welcome/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className={classes.Footer}>
             <span className={classes.author}>By Maxim Medyanik : <a className={classes.link} href="https://github.com/tvoygluk/quiz-o-frontend">github.com/tvoygluk/quiz-o-frontend</a></span>
-            <span className={classes.version}>v.0.7.0</span>
+            <span className={classes.version}>v.0.7.1</span>
         </footer>
     );
 };
